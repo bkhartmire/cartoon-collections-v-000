b5447ff0dd3beb5e3ceedeb9fbe3c0193d87f4a2
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-18 22:17:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(elements)# code an argument here
 end
 
 def long_planeteer_calls(calls)# code an argument here
-  calls.any? { |call| call.length < 4}
+  calls.any? { |call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
